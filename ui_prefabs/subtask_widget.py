54cd47f3341c06366f4db8154ce1711aeefda19d
itsru <itsrussellwhite@gmail.com>
2023-09-07 13:56:14 +0330

    I changed readme.md

--- ui_prefabs/subtask_widget.py
+++ ui_prefabs/subtask_widget.py
@@ -1,14 +1,26 @@
 import flet
+import flet as ft
 
 
 
 class SubTaskWidget (flet.Container):
-    def __init__ (self, *args, **kwargs):
+    def __init__ (self, name:str, done:bool, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        self.name = name
+        self.content = flet.Row(
+            controls=[
+                ft.Checkbox(
+                    value=done
+                ),
+                ft.Text(
+                    value=name
+                )
+            ]
+        )
 
 
 if __name__ == "__main__":
     def test (page:flet.Page):
-        page.add(SubTaskWidget())
+        page.add(SubTaskWidget(name="Go fuck yourself", done=True))
     
     flet.app(target=test)
